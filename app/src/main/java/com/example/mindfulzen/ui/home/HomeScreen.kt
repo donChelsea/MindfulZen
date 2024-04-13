decb50b4668f8304124f32f9ef1aa774466f48d2
Chelsea Katsidzira <1133280@iheartmedia.com>
2024-04-13 18:01:12 -0400

    set theme, font

---
+++ app/src/main/java/com/example/mindfulzen/ui/home/HomeScreen.kt
@@ -0,0 +1,4 @@
+package com.example.mindfulzen.ui.home
+
+class HomeScreen {
+}
--- app/src/main/java/com/example/mindfulzen/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mindfulzen/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DeepBlue,
+    secondary = AquaBlue,
+    tertiary = BlueViolet3
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = AquaBlue,
+    secondary = LightGreen1,
+    tertiary = BlueViolet1
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -63,8 +63,8 @@ fun MindfulZenTheme(
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
