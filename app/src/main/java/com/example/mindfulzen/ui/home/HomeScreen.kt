f7e39327ae534cab904177b4a4ac0c9d051a5bf9
Chelsea Katsidzira <1133280@iheartmedia.com>
2024-04-13 18:39:17 -0400

    created chips

--- app/src/main/java/com/example/mindfulzen/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mindfulzen/ui/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.mindfulzen.ui.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,15 +10,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.mindfulzen.ui.theme.ButtonBlue
+import com.example.mindfulzen.ui.theme.DarkerButtonBlue
 import com.example.mindfulzen.ui.theme.DeepBlue
 import com.example.mindfulzen.ui.theme.TextWhite
 
@@ -30,6 +41,7 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection()
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
         }
     }
 }
@@ -60,8 +72,38 @@ fun GreetingSection(
         Icon(
             imageVector = Icons.Outlined.Search,
             contentDescription = "Search",
-            tint = TextWhite,
+            tint = Color.White,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember { mutableIntStateOf(0) }
+
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ) {
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+            }
+        }
     }
 }
